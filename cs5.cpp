4fbdd60a8e695f082a20965e39fc4197b535c3f8
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-09 21:17:20 +0700

    menambahkan kategori BMI kurang dan normal

--- cs5.cpp
+++ cs5.cpp
@@ -22,29 +22,3 @@ string statusBMI(float bmi) {
      else if (bmi < 25) {
         return "Berat Badan Normal";
     } 
-    else if (bmi < 30) {
-        return "Berat Badan Kelebihan";
-    } 
-     else {
-        return "Obesitas";
-    }
-}
-
-int main(){
-     float berat, tinggi, bmi;
-
-    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
-
-     // Panggil prosedur input
-    inputData(berat, tinggi);
-
-    // Hitung BMI
-    bmi = hitungBMI(berat, tinggi);
-
-    cout << endl;
-    cout << "--- Hasil ---" << endl;
-    cout << "BMI Anda : " << bmi << endl;
-    cout << "Status   : " << statusBMI(bmi) << endl;
-
-    return 0;
-}
