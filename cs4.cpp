4b68c436ebcabbec585a9193d13d62dfded5db30
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-09 21:14:50 +0700

    membuat fungsi statusBMI untuk menentukan kategori BMI

---
+++ cs4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// Prosedur untuk memasukkan data
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
+// Fungsi untuk menghitung BMI
+float hitungBMI(float berat, float tinggi) {
+    return berat / (tinggi * tinggi);
+}
+
+// Fungsi untuk menentukan status BMI
+string statusBMI(float bmi) {
+    }
