8314618b88352624de665813616e7172d3eaf350
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-09 21:10:54 +0700

    menambahkan fungsi hitungBMI untuk menghitung BMI

---
+++ cs3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
+float hitungBMI(float berat, float tinggi) {
+    return berat / (tinggi * tinggi);
+}
