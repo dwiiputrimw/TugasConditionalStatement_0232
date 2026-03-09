ffe0cbef195b5e9e64ebd106ef29980298e10259
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-09 21:18:55 +0700

    menambahkan kategori BMI kelebihan dan obesitas

--- cs6.cpp
+++ cs6.cpp
@@ -28,23 +28,3 @@ string statusBMI(float bmi) {
      else {
         return "Obesitas";
     }
-}
-
-int main(){
-     float berat, tinggi, bmi;
-
-    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
-
-     // Panggil prosedur input
-    inputData(berat, tinggi);
-
-    // Hitung BMI
-    bmi = hitungBMI(berat, tinggi);
-
-    cout << endl;
-    cout << "--- Hasil ---" << endl;
-    cout << "BMI Anda : " << bmi << endl;
-    cout << "Status   : " << statusBMI(bmi) << endl;
-
-    return 0;
-}
